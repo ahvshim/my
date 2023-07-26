df2f56324c7f0b27ac86b266f057fd069cb75211
Ahmed <126220185+ahvshim@users.noreply.github.com>
2023-07-26 09:53:57 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,11 @@ document.getElementById('expense-form').addEventListener('submit', function (eve
 
   var formData = new FormData(event.target);
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://script.google.com/macros/s/AKfycbw23abnPCmOAyBkNbZP2tMUcHN407hUHVU8xpQeKB7lrLCIG0aIuy2bGkY-5BEvmqB2/exec', true);
+  xhr.open('POST', 'https://script.google.com/macros/s/AKfycbzT7Z8h5fI1V216X5LN8sLUF_DUDOd4Inpa5kjd4PB2_HwoWdsVT64pUtwC8K_nyqSY/exec', true);
 
   xhr.setRequestHeader('Expense-Type', 'application/x-www-form-urlencoded');
   xhr.onload = function () {
     if (xhr.status === 200) {
-      // Show success message and change color
       var formElement = document.getElementById('expense-form');
       formElement.innerHTML = '<p class="success-message">Expense Added!</p>';
       formElement.classList.add('expense-added');
